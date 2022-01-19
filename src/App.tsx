3c91c7334183c24e3f3d23713568fdc6de0f7e58
donkeedong <chase.steenbock@gmail.com>
2022-01-19 13:11:33 -0600

    added ant design and basic app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Typography } from 'antd';
+import * as React from 'react';
+
+const { Title } = Typography;
+
+const App = () => {
+  return <Title>Hello World!</Title>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import 'antd/dist/antd.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+
+var mountNode = document.getElementById('app');
+ReactDOM.render(<App />, mountNode);
